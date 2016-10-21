aa1eb14aee0e47a55564ee1c9e5494b3a68c2e1f
Lucia <luciainesromero@hotmail.com>
2016-10-21 16:22:50 -0300

    agrego coments

--- src/mmu.c
+++ src/mmu.c
@@ -28,9 +28,9 @@ void mmu_inicializar() {
 	page_directory[1] = PAGE_TABLE_2 | 0X3;
 	int i = 0;
 	for (  i = 2 ; i < 1024; i++ ) page_directory[i] = 0; 
-	for (  i = 896 ; i < 1024; i++ ) page_table_2[i] = 0; 
 	for (  i = 0 ; i < 1024; i++ ) page_table_1[i] = (i+1024*0) << 12 | 3;
 	for (  i = 0 ; i < 896; i++ ) page_table_2[i] = (i+1024*1) << 12 | 3; 
+	for (  i = 896 ; i < 1024; i++ ) page_table_2[i] = 0; // el resto de la tabla de pagina 2 en 0
 	
 }
 
@@ -47,7 +47,7 @@ void mmu_inicializar_dir_tarea() {
 	int* page_t3 = (int*) page_t3_dir;
 	
 	int i = 0;
-	for (  i = 0 ; i < 1024; i++ ) page_task_directory[i] = 0; 
+	for (  i = 0 ; i < 1024; i++ ) page_task_directory[i] = 0; // borro todo el directorio de pagina de la tarea
 
 	page_task_directory[0]  = page_t1_dir | 0x7;
 	page_task_directory[1] = page_t2_dir | 0x7; 
@@ -55,9 +55,9 @@ void mmu_inicializar_dir_tarea() {
 
 	for (  i = 0 ; i < 1024; i++ ) page_t1[i] = (i+1024*0) << 12 | 7;
 	for (  i = 0 ; i < 896; i++ ) page_t2[i] = (i+1024*1) << 12 | 7; 
-	for (  i = 896 ; i < 1024; i++ ) page_t2[i] = 0; 
+	for (  i = 896 ; i < 1024; i++ ) page_t2[i] = 0; // el resto de la tabla de pagina 2 en 0
 	
-	for (  i = 0 ; i < 1024; i++ ) page_t3[i] = 0; 
+	for (  i = 0 ; i < 1024; i++ ) page_t3[i] = 0; // pongo toda la tabla 3 en cero
 	page_t3[0] = 0x100007;
 	page_t3[1] = 0x101007;
 	page_t3[2] = 0x000007;
