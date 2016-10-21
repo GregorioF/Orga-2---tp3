0134d362bbed0200131f2d31e72a58f8b4d14d11
Gregorio FREIDIN <gfreidin@ws20.labo5>
2016-10-20 22:00:56 -0300

    clase d mierda

--- src/mmu.c
+++ src/mmu.c
@@ -31,4 +31,26 @@ void mmu_inicializar() {
 	for (  i = 0 ; i < 768; i++ ) page_table_2[i] = (i+1024*1) << 12 | 3; 
 	
 }
+
+void mmu_inicializar_dir_tarea() {
+	
+	int* page_task_directory = (int*) nextPage();
+	page_t1 = (int*) nextPage() | 0x7;
+	page_directory[0]  = page_t1;
+	page_t2 = (int*) nextPage() | 0x7;
+	page_directory[1] = page_t2; 
+	page_t3 = (int*) nextPage() | 0x7;
+	page_directory[256] = page_t3;
+	
+	int i = 0;
+	for (  i = 2 ; i < 1024; i++ ) page_directory[i] = 0; 
+	for (  i = 0 ; i < 1024; i++ ) page_t1[i] = (i+1024*0) << 12 | 7;
+	for (  i = 0 ; i < 768; i++ ) page_t2[i] = (i+1024*1) << 12 | 7; 
+	
+	page_t3[0] = 0x100007;
+	page_t3[1] = 0x101007;
+	page_t3[2] = 0x000007;
+	
+		
+}
  
