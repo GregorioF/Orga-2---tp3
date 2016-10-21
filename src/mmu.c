9806af580cfca28785206dfc8e2beb2f40f7969d
Lucia <luciainesromero@hotmail.com>
2016-10-21 16:21:01 -0300

    andando perfecto

--- src/mmu.c
+++ src/mmu.c
@@ -15,7 +15,7 @@ int* page_directory = (int*)PAGE_DIRECTORY;
 int* page_table_1 = (int*)PAGE_TABLE_1;
 int* page_table_2 = (int*)PAGE_TABLE_2;
 
-int PPL = 0x30000;
+int PPL = 0x31000;
 
 int nextPage(){
 		PPL += 0x1000;
@@ -28,36 +28,40 @@ void mmu_inicializar() {
 	page_directory[1] = PAGE_TABLE_2 | 0X3;
 	int i = 0;
 	for (  i = 2 ; i < 1024; i++ ) page_directory[i] = 0; 
+	for (  i = 896 ; i < 1024; i++ ) page_table_2[i] = 0; 
 	for (  i = 0 ; i < 1024; i++ ) page_table_1[i] = (i+1024*0) << 12 | 3;
-	for (  i = 0 ; i < 768; i++ ) page_table_2[i] = (i+1024*1) << 12 | 3; 
+	for (  i = 0 ; i < 896; i++ ) page_table_2[i] = (i+1024*1) << 12 | 3; 
 	
 }
 
 void mmu_inicializar_dir_tarea() {
 	
-	int* page_task_directory = (int*) nextPage();
+	int* page_task_directory = (int*) 0x31000;
 
 	int page_t1_dir =  nextPage();
-	page_task_directory[0]  = page_t1_dir | 0x7;
-
 	int page_t2_dir = nextPage();
-	page_task_directory[1] = page_t2_dir | 0x7; 
-
 	int page_t3_dir = nextPage();
-	page_task_directory[256] = page_t3_dir | 0x7;
 
 	int* page_t1 = (int*) page_t1_dir;
 	int* page_t2 = (int*) page_t2_dir;
 	int* page_t3 = (int*) page_t3_dir;
 	
 	int i = 0;
-	for (  i = 2 ; i < 1024; i++ ) page_directory[i] = 0; 
+	for (  i = 0 ; i < 1024; i++ ) page_task_directory[i] = 0; 
+
+	page_task_directory[0]  = page_t1_dir | 0x7;
+	page_task_directory[1] = page_t2_dir | 0x7; 
+	page_task_directory[256] = page_t3_dir | 0x7;
+
 	for (  i = 0 ; i < 1024; i++ ) page_t1[i] = (i+1024*0) << 12 | 7;
-	for (  i = 0 ; i < 768; i++ ) page_t2[i] = (i+1024*1) << 12 | 7; 
+	for (  i = 0 ; i < 896; i++ ) page_t2[i] = (i+1024*1) << 12 | 7; 
+	for (  i = 896 ; i < 1024; i++ ) page_t2[i] = 0; 
 	
+	for (  i = 0 ; i < 1024; i++ ) page_t3[i] = 0; 
 	page_t3[0] = 0x100007;
 	page_t3[1] = 0x101007;
 	page_t3[2] = 0x000007;
+	for (  i = 3 ; i < 1024; i++ ) page_t3[i] = 0; 
 	
 	unsigned char* codigo = (unsigned char*) 0x10000;
 	unsigned char* destino = (unsigned char*) 0x100000;
