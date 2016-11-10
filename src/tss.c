144296c8ee61f7bf4afa4248d7363163bc558212
staboh@dc.uba.ar <sebi_282@hotmail.com>
2016-11-10 00:51:49 -0300

    Mini comentario

--- src/tss.c
+++ src/tss.c
@@ -7,7 +7,7 @@
 
 #include "tss.h"
 #include "gdt.h"
-#include "mmu.h" 
+#include "mmu.h"
 
 tss tarea_inicial;
 tss tarea_idle;
@@ -19,10 +19,10 @@ int dir_tareas []  = {0x31000, 0x32000, 0x33000, 0x34000, 0x35000, 0x36000, 0x37
 void actualizar_gdt(){
 	int i = 0;
 	int j = 0;
-	
-	
+
+
     gdt[TAREA_INICIAL] = (gdt_entry) {
-        (unsigned short)    0x67,         /* limit[0:15]  */
+        (unsigned short)    0x67,         /* limit[0:15], tamaño - 1 = 104 - 1 = 103 = 0x67  */
         (unsigned short)    (unsigned long int)&tarea_inicial & 0x0000ffff,         /* base[0:15]   */
         (unsigned char)     ((unsigned long int)&tarea_inicial & 0x00ff0000) >> 16,           /* base[23:16]  */
         (unsigned char)     0x09,           /* type         */
@@ -51,10 +51,10 @@ void actualizar_gdt(){
         (unsigned char)     0x00,           /* g            */
         (unsigned char)     ((unsigned long int)&tarea_idle & 0xff000000) >> 24,           /* base[31:24]  */
     };
-	
-	
+
+
 	for(i  = 25, j = 0 ; i < 33 && j  < 8 ; i++, j++){
-		
+
 			gdt[i] = (gdt_entry){
         (unsigned short)    0x67,         /* limit[0:15]  */
         (unsigned short)    ((unsigned long int)&tss_navios[j]) & 0x0000ffff,         /* base[0:15]   */
@@ -69,9 +69,9 @@ void actualizar_gdt(){
         (unsigned char)     0x01,           /* db           */
         (unsigned char)     0x00,           /* g            */
         (unsigned char)     (((unsigned long int)&tss_navios[j]) & 0xff000000) >> 24,           /* base[31:24]  */
-		};    
+		};
 	}
-	
+
 	for(i = 33, j = 0; i < 41 && j < 8 ; i ++, j++ ){
 		gdt[i]= (gdt_entry) {
         (unsigned short)    0x67,         /* limit[0:15]  */
@@ -88,7 +88,7 @@ void actualizar_gdt(){
         (unsigned char)     0x00,           /* g            */
         (unsigned char)     ((unsigned long int)&tss_banderas[j] & 0xff000000) >> 24,           /* base[31:24]  */
 		};
-	
+
 	}
 }
 
@@ -122,17 +122,17 @@ void tss_inicializar() {
         tss_navios[i].eip = 0x40000000;
         tss_navios[i].esp = 0x40001C00;
         tss_navios[i].ebp = 0x40001C00;
-        tss_navios[i].eflags = 0x202;  
-       
+        tss_navios[i].eflags = 0x202;
+
         tss_navios[i].esp0 = nextPage() + 0x1000;
         tss_navios[i].ss0 = GDT_DAT_L0 << 3;
-       
+
 
         tss_navios[i].cs = GDT_COD_L3 << 3 | 0x3;
-        tss_navios[i].ds = GDT_DAT_L3 << 3 | 0x3; 
-        tss_navios[i].fs = GDT_DAT_L3 << 3 | 0x3; 
+        tss_navios[i].ds = GDT_DAT_L3 << 3 | 0x3;
+        tss_navios[i].fs = GDT_DAT_L3 << 3 | 0x3;
         tss_navios[i].ss = GDT_DAT_L3 << 3 | 0x3;
-       
+
         tss_navios[i].cr3 = dir_tareas[i];
 
 
@@ -148,31 +148,30 @@ void tss_inicializar() {
         tss_navios[i].unused5 = 0;
         tss_navios[i].unused4=0;
         tss_navios[i].es = GDT_DAT_L3 << 3 | 3;
-        
-        
-        
-        
+
+
+
+
 		mmu_inicializar_dir_tarea(i, dir_tareas[i])	 ;
-				
+
 		tss_banderas[i].eip = *((unsigned int*) (0x11FFC + dosk*i)) + 0x40000000; // MEGA BRIAN GATUBELA
-		
+
 		tss_banderas[i].esp = 0x40001FFC;
         tss_banderas[i].ebp = 0x40001FFC;
-        tss_banderas[i].eflags = 0x202;  
-       
+        tss_banderas[i].eflags = 0x202;
+
         tss_banderas[i].esp0 = nextPage()+ 0x1000;
         tss_banderas[i].ss0 = GDT_DAT_L0 << 3;
-       
+
         tss_banderas[i].cs = GDT_COD_L3 << 3 | 0x3;
         tss_banderas[i].ds = GDT_DAT_L3 << 3 | 0x3;
         tss_banderas[i].ss = GDT_DAT_L3 << 3 | 0x3;
         tss_banderas[i].cr3 = dir_tareas[i];
-    	
-        
-    }
-    
-    
-	
-	
-}
 
+
+    }
+
+
+
+
+}
